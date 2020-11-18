12b1b51fdce928828e4fa86585652c125d92355c
Andrea Parodi <andrea@parro.it>
2020-11-18 15:15:26 +0100

    customizable wrf_var.txt

--- cli.go
+++ cli.go
@@ -111,6 +111,7 @@ func buildWRFDir(fs *fsutil.Transaction, start, end time.Time, step int) {
 
 	dtStart := start.Add(3 * time.Duration(step-3) * time.Hour)
 	dtEnd := dtStart.Add(3 * time.Hour)
+
 	if step == 3 {
 		dtEnd = end
 		wrfPrg = wrfPrgMainRun
@@ -137,7 +138,18 @@ func buildWRFDir(fs *fsutil.Transaction, start, end time.Time, step int) {
 		},
 	)
 
-	fs.Save(wrfDir.Join("wrf_var.txt"), []byte("\n"))
+	wrfvar := ""
+	switch step {
+	case 1:
+		wrfvar = "wrf_var.txt.wrf_01"
+	case 2:
+		wrfvar = "wrf_var.txt.wrf_02"
+	case 3:
+		wrfvar = "wrf_var.txt.wrf_03"
+	}
+
+	fs.Copy(conf.Config.Folders.NamelistsDir.Join(wrfvar), wrfDir.Join(wrfvar))
+
 	fs.LinkAbs(wrfPrg.Join("main/wrf.exe"), wrfDir.Join("wrf.exe"))
 	fs.LinkAbs(wrfPrg.Join("run/LANDUSE.TBL"), wrfDir.Join("LANDUSE.TBL"))
 	fs.LinkAbs(wrfPrg.Join("run/ozone_plev.formatted"), wrfDir.Join("ozone_plev.formatted"))
