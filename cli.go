c0e551e9ac1f7e39bc42903f30acd9c19dd33759
Andrea Parodi <andrea@parro.it>
2020-11-24 10:18:27 +0100

    removed .err suffix from ob.ascii files

--- cli.go
+++ cli.go
@@ -235,7 +235,7 @@ func buildDADirInDomain(fs *fsutil.Transaction, start, end time.Time, step, doma
 	// link observations
 	assimDateS := assimDate.Format("2006010215")
 	fs.LinkAbs(observationsDir.JoinF("ob.radar.%s", assimDateS), daDir.Join("ob.radar"))
-	fs.LinkAbs(observationsDir.JoinF("ob.ascii.%s.err", assimDateS), daDir.Join("ob.ascii"))
+	fs.LinkAbs(observationsDir.JoinF("ob.ascii.%s", assimDateS), daDir.Join("ob.ascii"))
 }
 
 func runDAStep(fs *fsutil.Transaction, start time.Time, step int) {
