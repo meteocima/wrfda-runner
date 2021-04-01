fc60a910f5ef1c9b2a95ad7bde08f4329ab051e9
Andrea Parodi <andrea@parro.it>
2021-04-01 12:34:43 +0200

    reduced procs

--- common/common.go
+++ common/common.go
@@ -1,5 +1,24 @@
 package common
 
+// version limited to 5 lrz nodes
+
+// // GeogridProcCount ...
+// var GeogridProcCount = "84"
+//
+// // MetgridProcCount ...
+// var MetgridProcCount = "84"
+//
+// // WrfstepProcCount ...
+// var WrfstepProcCount = "132"
+//
+// // WrfdaProcCount ...
+// var WrfdaProcCount = "42"
+//
+// // RealProcCount ...
+// var RealProcCount = "42"
+
+// version limited to 4 lrz nodes - can run on interactive partition
+
 // GeogridProcCount ...
 var GeogridProcCount = "84"
 
@@ -7,10 +26,10 @@ var GeogridProcCount = "84"
 var MetgridProcCount = "84"
 
 // WrfstepProcCount ...
-var WrfstepProcCount = "132"
+var WrfstepProcCount = "110"
 
 // WrfdaProcCount ...
-var WrfdaProcCount = "42"
+var WrfdaProcCount = "36"
 
 // RealProcCount ...
 var RealProcCount = "42"
